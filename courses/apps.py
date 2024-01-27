2d4c2e7b01d8fa12a559fe55a5697573e633f33b
ifounder <biifounder@gmail.com>
2024-01-27 12:01:25 +0000

    6

--- courses/apps.py
+++ courses/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class AzureContentConfig(AppConfig):
+class Courses(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'courses'
